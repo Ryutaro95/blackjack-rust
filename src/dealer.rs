862fa62ecaf3d541e7f4cf0dfb2be2832f330e7a
Ryutaro95 <ryutaro9595@gmail.com>
2022-02-26 06:19:03 +0900

    Create Dealer struct.

--- src/dealer.rs
+++ src/dealer.rs
@@ -1,25 +1,19 @@
-use crate::card::{Card, Number::*};
-use crate::deck::Deck;
-use crate::score::Score;
+use crate::{deck::Deck, score::Score, card::{Card, Number::*}, player::Person};
 
-pub trait Person {
-    fn draw(&mut self, deck: &mut Deck);
-    fn result_score(&self) -> Score;
-}
-
-#[derive(Debug, Default)]
-pub struct Player {
+#[derive(Default, Debug)]
+pub struct Dealer {
     pub hands: Vec<Card>,
     pub score: Score,
 }
 
-impl Person for Player {
+impl Person for Dealer {
     fn draw(&mut self, deck: &mut Deck) {
         for _i in 0..2 {
             // It never fails.
             self.hands.push(deck.draw().unwrap());
         }
     }
+
     fn result_score(&self) -> Score {
         let mut score = 0 as u8;
         for card in self.hands.iter() {
